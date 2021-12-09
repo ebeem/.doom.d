82d5971fbda35e7067aa8b56a0efedf16dfce7a0
Almarhoon Ibraheem <ibeem@outlook.com>
2021-12-09 18:00:54 +0300

    update

---
+++ custom.el
@@ -0,0 +1,65 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#292A30" "#FC6A5D" "#67B7A4" "#D0BF68" "#5DD8FF" "#D0A8FF" "#8abeb7" "#FFFFFF"])
+ '(custom-safe-themes
+   '("7eea50883f10e5c6ad6f81e153c640b3a288cd8dc1d26e4696f7d40f754cc703" "c2aeb1bd4aa80f1e4f95746bda040aafb78b1808de07d340007ba898efa484f5" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "97db542a8a1731ef44b60bc97406c1eb7ed4528b0d7296997cbb53969df852d6" "8146edab0de2007a99a2361041015331af706e7907de9d6a330a3493a541e5a6" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "da186cce19b5aed3f6a2316845583dbee76aea9255ea0da857d1c058ff003546" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
+ '(exwm-floating-border-color "#161616")
+ '(fci-rule-color "#5c5e5e")
+ '(highlight-tail-colors ((("#2f383b") . 0) (("#32383d") . 20)))
+ '(hl-todo-keyword-faces
+   '(("TODO" . "#dc752f")
+     ("NEXT" . "#dc752f")
+     ("THEM" . "#2d9574")
+     ("PROG" . "#4f97d7")
+     ("OKAY" . "#4f97d7")
+     ("DONT" . "#f2241f")
+     ("FAIL" . "#f2241f")
+     ("DONE" . "#86dc2f")
+     ("NOTE" . "#b1951d")
+     ("KLUDGE" . "#b1951d")
+     ("HACK" . "#b1951d")
+     ("TEMP" . "#b1951d")
+     ("FIXME" . "#dc752f")
+     ("XXX+" . "#dc752f")
+     ("\\?\\?\\?+" . "#dc752f")))
+ '(jdee-db-active-breakpoint-face-colors (cons "#0d0d0d" "#5DD8FF"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#0d0d0d" "#67B7A4"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#0d0d0d" "#6C7986"))
+ '(objed-cursor-color "#FC6A5D")
+ '(package-selected-packages
+   '(org-superstar spacemacs-theme vterm emms simple-mpc bongo window-number visual-fill-column visual-fill jupyter with-editor))
+ '(pdf-view-midnight-colors (cons "#FFFFFF" "#292A30"))
+ '(rustic-ansi-faces
+   ["#292A30" "#FC6A5D" "#67B7A4" "#D0BF68" "#5DD8FF" "#D0A8FF" "#8abeb7" "#FFFFFF"])
+ '(vc-annotate-background "#292A30")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#67B7A4")
+    (cons 40 "#8ab990")
+    (cons 60 "#adbc7c")
+    (cons 80 "#D0BF68")
+    (cons 100 "#dfaf5a")
+    (cons 120 "#ee9f4c")
+    (cons 140 "#FD8F3F")
+    (cons 160 "#ee977f")
+    (cons 180 "#df9fbf")
+    (cons 200 "#D0A8FF")
+    (cons 220 "#de93c9")
+    (cons 240 "#ed7e93")
+    (cons 260 "#FC6A5D")
+    (cons 280 "#d86d67")
+    (cons 300 "#b47171")
+    (cons 320 "#90757b")
+    (cons 340 "#5c5e5e")
+    (cons 360 "#5c5e5e")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
